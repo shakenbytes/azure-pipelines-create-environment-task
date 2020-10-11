206ac8225f60a9e5cecb406ab96a0f90a704514e
Frankely Diaz <frankely.diaz@alliant.com>
2020-10-10 21:45:47 -0600

    Update publish
    Update publish

--- publish.sh
+++ publish.sh
@@ -1,1 +1,1 @@
-tfx extension publish --manifest-globs vss-extension.json --share-with shaken-bytes --auth-type pat -token $PAT
+tfx extension publish --manifest-globs vss-extension.json --share-with shaken-bytes --auth-type pat --token $PAT --no-prompt
